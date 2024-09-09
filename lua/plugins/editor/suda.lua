5f5b1e26a3afbb876179e87ce294ba5fd2dfef22
groveer <groveer@outlook.com>
2024-09-09 13:58:23 +0800

    fix: suda cannot work in insert mode

--- lua/plugins/editor/suda.lua
+++ lua/plugins/editor/suda.lua
@@ -1,8 +1,8 @@
 return {
   "lambdalisue/suda.vim",
   keys = {
-    { "<A-s>", "<CMD>SudaWrite<CR>", "n", desc = "Suda Save file using sudo" },
-    { "<A-s>", "<Esc><CMD>SudaWrite<CR>", "i", desc = "Suda Save file using sudo" },
+    { "<A-s>", "<CMD>SudaWrite<CR>", mode = "n", desc = "Suda Save file using sudo" },
+    { "<A-s>", "<Esc><CMD>SudaWrite<CR>", mode = "i", desc = "Suda Save file using sudo" },
   },
   cmd = { "SudaRead", "SudaWrite" },
   config = function()
