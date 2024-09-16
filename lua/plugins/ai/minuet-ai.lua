d7d58cf2b5ca70edf689652a5becaf83d87482cf
groveer <groveer@outlook.com>
2024-09-17 00:21:37 +0800

    update `minuet-ai` config, add `deepseek` ai provider

--- lua/plugins/ai/minuet-ai.lua
+++ lua/plugins/ai/minuet-ai.lua
@@ -3,15 +3,31 @@ return {
   event = "InsertEnter",
   opts = function()
     return {
-      notify = false,
-      provider = "openai_compatible",
+      notify = "warn",
+      request_timeout = 30,
+      provider = "openai_fim_compatible",
       provider_options = {
         openai_compatible = {
-          end_point = "http://chat.groveer.com:11434/v1/chat/completions",
-          model = "codellama",
+          end_point = "http://127.0.0.1:11434/v1/chat/completions",
+          model = "llama3.1",
           name = "Ollama",
           stream = true,
-          api_key = "OPENAI_API_KEY",
+          api_key = "OPENAI_API_KEY", -- must be set, can set any value
+          optional = {
+            stop = "\n\n",
+            max_tokens = 128,
+          },
+        },
+        openai_fim_compatible = {
+          model = "deepseek-coder",
+          end_point = "https://api.deepseek.com/beta/completions",
+          name = "Deepseek",
+          api_key = "DEEPSEEK_API_KEY",
+          stream = true,
+          optional = {
+            stop = "\n\n",
+            max_tokens = 128,
+          },
         },
       },
     }
