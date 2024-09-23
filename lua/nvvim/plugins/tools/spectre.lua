f2210f0cf5ee2f2e0f02659282b4e148f57873c2
groveer <groveer@outlook.com>
2024-09-23 18:18:15 +0800

    feat: add `word regexp` option for nvim-spectre

--- lua/nvvim/plugins/tools/spectre.lua
+++ lua/nvvim/plugins/tools/spectre.lua
@@ -32,6 +32,41 @@ return {
       desc = "Search on current file",
     },
   },
+  opts = {
+    find_engine = {
+      -- rg is map with finder_cmd
+      ["rg"] = {
+        cmd = "rg",
+        -- default args
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+        },
+        options = {
+          ["ignore-case"] = {
+            value = "--ignore-case",
+            icon = "[I]",
+            desc = "ignore case",
+          },
+          ["hidden"] = {
+            value = "--hidden",
+            desc = "hidden file",
+            icon = "[H]",
+          },
+          ["word"] = {
+            value = "--word-regexp",
+            desc = "word regexp",
+            icon = "[W]",
+          },
+          -- you can put any rg search option you want here it can toggle with
+          -- show_option function
+        },
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
