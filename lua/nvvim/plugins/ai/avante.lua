2854d11f0f8f0e16b69f9bc65c4e8e38bfdd9b47
groveer <groveer@outlook.com>
2025-04-27 14:42:20 +0800

    chore(avante): rm not need prompt

--- lua/nvvim/plugins/ai/avante.lua
+++ lua/nvvim/plugins/ai/avante.lua
@@ -1,31 +1,3 @@
--- NOTE: most templates are inspired from ChatGPT.nvim -> chatgpt-actions.json
-local avante_grammar_correction = "将文本更正为标准英语，但保持内部所有代码块完好无损。"
-local avante_keywords = "从以下文本中提取主要关键词"
-local avante_code_readability_analysis = [[
-      你必须识别代码片段中的任何可读性问题。
-      需要考虑的一些可读性问题：
-      - 命名不明确
-      - 目的不明确
-      - 多余或明显的注释
-      - 缺乏注释
-      - 长或复杂的单行代码
-      - 嵌套过多
-      - 变量名过长
-      - 命名和代码风格不一致
-      - 代码重复
-      你可能会发现其他问题。用户提交了一小段来自较大文件的代码。
-      仅列出有可读性问题的行，格式为 <line_num>|<issue and proposed solution>
-      如果代码没有问题，仅回复：<OK>
-    ]]
-local avante_optimize_code = "优化以下代码"
-local avante_summarize = "总结以下文本"
-local avante_translate = "将此翻译为中文，但保持内部的所有代码块完好无损"
-local avante_explain_code = "解释以下代码"
-local avante_complete_code = "完成以下用 " .. vim.bo.filetype .. " 编写的代码"
-local avante_add_docstring = "为以下代码添加文档字符串"
-local avante_fix_bugs = "修复以下代码中的错误（如果有）"
-local avante_add_tests = "为以下代码实现测试"
-
 local build_cmd = not require("nvvim.configs").is_windows and "make"
   or "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
 
@@ -56,94 +28,6 @@ return {
       desc = "avante: edit",
       mode = { "n", "v" },
     },
-    {
-      "<leader>ag",
-      function()
-        require("avante").setup()
-        require("avante.api").ask({ question = avante_grammar_correction })
-      end,
-      mode = { "n", "v" },
-      desc = "Grammar Correction(ask)",
-    },
-    {
-      "<leader>ak",
-      function()
-        require("avante.api").ask({ question = avante_keywords })
-      end,
-      mode = { "n", "v" },
-      desc = "Keywords(ask)",
-    },
-    {
-      "<leader>al",
-      function()
-        require("avante.api").ask({ question = avante_code_readability_analysis })
-      end,
-      mode = { "n", "v" },
-      desc = "Code Readability Analysis(ask)",
-    },
-    {
-      "<leader>ao",
-      function()
-        require("avante.api").ask({ question = avante_optimize_code })
-      end,
-      mode = { "n", "v" },
-      desc = "Optimize Code(ask)",
-    },
-    {
-      "<leader>am",
-      function()
-        require("avante.api").ask({ question = avante_summarize })
-      end,
-      desc = "Summarize text(ask)",
-    },
-    {
-      "<leader>an",
-      function()
-        require("avante.api").ask({ question = avante_translate })
-      end,
-      mode = { "n", "v" },
-      desc = "Translate text(ask)",
-    },
-    {
-      "<leader>ax",
-      function()
-        require("avante.api").ask({ question = avante_explain_code })
-      end,
-      mode = { "n", "v" },
-      desc = "Explain Code(ask)",
-    },
-    {
-      "<leader>ac",
-      function()
-        require("avante.api").ask({ question = avante_complete_code })
-      end,
-      mode = { "n", "v" },
-      desc = "Complete Code(ask)",
-    },
-    {
-      "<leader>ad",
-      function()
-        require("avante.api").ask({ question = avante_add_docstring })
-      end,
-      mode = { "n", "v" },
-      desc = "Docstring(ask)",
-    },
-    {
-      "<leader>ab",
-      function()
-        require("avante.api").ask({ question = avante_fix_bugs })
-      end,
-      mode = { "n", "v" },
-      desc = "Fix Bugs(ask)",
-    },
-    {
-      "<leader>au",
-      function()
-        require("avante.api").ask({ question = avante_add_tests })
-      end,
-      mode = { "n", "v" },
-      desc = "Add Tests(ask)",
-    },
   },
   cmd = "AvanteAsk",
   version = false,
@@ -157,6 +41,7 @@ return {
       enabled_claude_text_editor_tool_mode = true,
     },
     provider = "copilot",
+    -- provider = "openai",
     cursor_applying_provider = "groq",
     copilot = {
       -- model = "claude-3.5-sonnet",
@@ -170,13 +55,6 @@ return {
         endpoint = "https://api.groq.com/openai/v1/",
         model = "llama-3.3-70b-versatile",
         max_completion_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
-      },
-      deepseek = {
-        __inherited_from = "openai",
-        endpoint = "http://localhost:8000/v1",
-        api_key_name = "DEEPSEEK_API_KEY",
-        model = "deepseek-r1",
-        max_tokens = 10240,
       },
     },
   },
