0334549e4052ac0fec30934f75ce9fd73383d2c5
groveer <groveer@outlook.com>
2025-06-29 23:44:59 +0800

    chore: set fzf config

--- lua/nvvim/plugins/tools/fzf-lua.lua
+++ lua/nvvim/plugins/tools/fzf-lua.lua
@@ -77,6 +77,61 @@ return {
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    -- 全局设置适用于所有 fzf-lua 命令
+    global = {
+      file_ignore_patterns = {
+        "node_modules/.*", -- 忽略 node_modules 目录
+        "%.git/.*", -- 忽略 .git 目录
+        "build/.*", -- 忽略 build 目录
+        "dist/.*", -- 忽略 dist 目录
+        "%.idea/.*", -- 忽略 .idea 目录
+        "%.vscode/.*", -- 忽略 .vscode 目录
+      },
+      rg_glob = true, -- 启用 ripgrep 的 glob 模式
+    },
+
+    grep = {
+      -- 专门针对 live_grep 的设置
+      live_grep = {
+        rg_opts = "--hidden --follow --smart-case", -- ripgrep 基础选项
+        rg_extra_args = function()
+          -- 动态构建排除目录参数
+          local excludes = {
+            "--glob=!**/node_modules/**",
+            "--glob=!**/.git/**",
+            "--glob=!**/build/**",
+            "--glob=!**/dist/**",
+            "--glob=!**/.idea/**",
+            "--glob=!**/vendor/**",
+            "--glob=!**/__pycache__/**",
+          }
+          return excludes
+        end,
+      },
+
+      -- 普通 grep 设置
+      grep = {
+        rg_extra_args = {
+          "--glob=!**/node_modules/**",
+          "--glob=!**/.git/**",
+        },
+      },
+    },
+
+    files = {
+      -- 文件搜索的忽略设置
+      file_ignore_patterns = {
+        "^node_modules/",
+        "^%.git/",
+        "^build/",
+        "^dist/",
+        "%.o$",
+        "%.a$",
+        "%.so$",
+        "%.dll$",
+        "%.exe$",
+      },
+    },
     keymap = {
       builtin = {
         ["<C-f>"] = "preview-page-down",
