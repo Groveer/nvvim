761d921a5ea8265e3d9a326a907838fb4918270b
groveer <groveer@outlook.com>
2025-08-28 17:49:51 +0800

    fix(ui): add missing dependency for nvim-web-devicons
    Added "NvChad/ui" as a dependency to ensure proper icon setup in devicons.
    补充了 "NvChad/ui" 依赖，确保 devicons 图标能正确设置。
    
    Log: 补充 nvim-web-devicons 的依赖项

--- lua/nvvim/plugins/ui/nvim-web-devicons.lua
+++ lua/nvvim/plugins/ui/nvim-web-devicons.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-tree/nvim-web-devicons",
+    dependencies = { "NvChad/ui" },
     config = function(_, opts)
       opts.override = require("nvchad.icons.devicons")
       require("nvim-web-devicons").setup(opts)
