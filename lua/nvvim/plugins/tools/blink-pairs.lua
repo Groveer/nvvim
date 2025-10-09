3b6a26c20c808f698b6bc3815a3664e3be7a4bc1
groveer <groveer@outlook.com>
2025-10-09 23:26:28 +0800

    use Saghen/blink.download to download prebuilt binaries

--- lua/nvvim/plugins/tools/blink-pairs.lua
+++ lua/nvvim/plugins/tools/blink-pairs.lua
@@ -3,15 +3,13 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   version = "*", -- (recommended) only required with prebuilt binaries
   -- download prebuilt binaries from github releases
-  -- dependencies = "saghen/blink.download",
+  dependencies = "saghen/blink.download",
   -- OR build from source, requires nightly:
   -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  build = "cargo build --release",
+  -- build = "cargo build --release",
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
-  --- @module 'blink.pairs'
-  --- @type blink.pairs.Config
   opts = {
     mappings = {
       -- you can call require("blink.pairs.mappings").enable()
