edc143d31cedd6a5339f0ee77ef9e4b1b48bd549
groveer <groveer@outlook.com>
2026-03-01 21:51:14 +0800

    chore(codecompanion): update api platform config

--- lua/nvvim/plugins/codecompanion.lua
+++ lua/nvvim/plugins/codecompanion.lua
@@ -24,7 +24,6 @@ return {
     opts = {
       language = "Chinese",
       send_code = true,
-      completion_provider = "blink",
     },
     prompt_library = {
       markdown = {
@@ -37,27 +36,24 @@ return {
       chat = {
         start_in_insert_mode = true,
       },
-      action_palette = {
-        provider = "fzf_lua",
-      },
     },
     adapters = {
       http = {
-        uniontech = function()
+        groveer = function()
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
-              url = "https://ai.uniontech.com/api",
-              api_key = "UT_KEY",
+              url = "https://axonhub.groveer.com",
+              api_key = "AXONHUB_API_KEY",
             },
             schema = {
               model = {
-                default = "kimi-k2",
+                default = "minimaxai/minimax-m2.5",
               },
               think = {
-                default = false,
+                default = true,
               },
               num_ctx = {
-                default = 1638400,
+                default = 200 * 1024,
               },
             },
           })
@@ -66,12 +62,10 @@ return {
     },
     interactions = {
       inline = {
-        adapter = {
-          name = "uniontech",
-          model = "kimi-k2",
-        },
+        adapter = "copilot",
       },
       chat = {
+        adapter = "groveer",
         keymaps = {
           clear = {
             modes = { n = "gX" },
@@ -80,30 +74,6 @@ return {
         opts = {
           completion_provider = "blink", -- blink|cmp|coc|default
         },
-        adapter = {
-          name = "uniontech",
-          model = "kimi-k2",
-          -- model = "claude-3.7-sonnet",
-          -- model = "claude-sonnet-4",
-          -- model = "gemini-2.5-pro",
-        },
-        slash_commands = {
-          ["buffer"] = {
-            opts = { provider = "fzf_lua" },
-          },
-          ["fetch"] = {
-            opts = { provider = "fzf_lua" },
-          },
-          ["file"] = {
-            opts = { provider = "fzf_lua" },
-          },
-          ["help"] = {
-            opts = { provider = "fzf_lua" },
-          },
-          ["symbols"] = {
-            opts = { provider = "fzf_lua" },
-          },
-        },
         tools = {
           opts = {
             auto_submit_errors = true, -- Send any errors to the LLM automatically?
@@ -111,14 +81,14 @@ return {
           },
         },
       },
+      cmd = {
+        adapter = "copilot",
+      },
     },
     extensions = {
       history = {
         enabled = true, -- defaults to true
         dir_to_save = vim.fn.stdpath("data") .. "/history/codecompanion-history",
-        opts = {
-          picker = "fzf_lua", --- ("telescope", "snacks", "fzf-lua", or "default")
-        },
       },
     },
   },
@@ -129,7 +99,6 @@ return {
   },
 
   config = function(_, opts)
-    vim.g.codecompanion_auto_tool_mode = true
     local history_dir = vim.fn.stdpath("data") .. "/history"
     if vim.fn.isdirectory(history_dir) == 0 then
       vim.fn.mkdir(history_dir, "p")
