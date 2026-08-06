9c0f9440b7ac374ee1d0fcd61ff465c638d4ab08
groveer <groveer@outlook.com>
2026-08-06 13:24:56 +0800

    update blink.pairs plugin config

--- lua/nvvim/plugins/blink.pairs.lua
+++ lua/nvvim/plugins/blink.pairs.lua
@@ -2,11 +2,10 @@ return {
   "saghen/blink.pairs",
   event = { "BufReadPost", "BufNewFile" },
   version = "*", -- (recommended) only required with prebuilt binaries
-  -- download prebuilt binaries from github releases
-  -- dependencies = "saghen/blink.download",
-  -- OR build from source, requires nightly:
-  -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  build = "cargo build --release",
+  dependencies = "saghen/blink.lib",
+  build = function()
+    require("blink.pairs").download():pwait(60000)
+  end,
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
